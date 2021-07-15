98ecf32b2b14d2c6da5ac891149d8c7a6c6bce2a
Gabriella0Oliveira <maria0gabriella@gmail.com>
2021-07-15 13:27:15 -0300

    Implementing the paintComponent() function.

--- SpaceGame.java
+++ SpaceGame.java
@@ -1,8 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class SpaceGame extends JPanel implements Runnable {
 
+    Spaceship spaceship;
+
     public SpaceGame() {
+
+        spaceship = new Spaceship();
+
         Thread game = new Thread(this);
         game.start();  //this will call run() method
     }
@@ -17,7 +23,19 @@ public class SpaceGame extends JPanel implements Runnable {
     }
 
     private void update() {
-        System.out.println("updating"); // just checking if it updates
+        //System.out.println("updating"); // just checking if it updates
+    }
+
+    @Override
+    public void paintComponent(Graphics g2) {
+        super.paintComponent(g2);
+
+        Graphics2D g = (Graphics2D) g2.create();
+
+        spaceship.paint(g);
+
+        /*g.setColor(Color.white);
+        g.fillRect(0,0,50,50); testing */
     }
 
     private void sleep() {
