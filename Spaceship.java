9a576e112032542090503c26d9ec5d720a489032
Gabriella0Oliveira <maria0gabriella@gmail.com>
2021-07-15 13:25:25 -0300

    Implementing the paint() method.

--- Spaceship.java
+++ Spaceship.java
@@ -6,7 +6,7 @@ import java.io.File;
 public class Spaceship {
 
     private BufferedImage image;
-    private int x; //position
+    private final int x; //position
     private int speed;
 
     public Spaceship(){
@@ -23,7 +23,7 @@ public class Spaceship {
     }
 
     public void paint(Graphics2D g){
-
+        g.drawImage(image,x,550,x+ 100 , 650, 0, 0,image.getWidth(),image.getHeight(),null );
     }
 
     public void move(){
